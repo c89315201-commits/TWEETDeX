b77f42772b72af3bf86d4adc26218af910d06f43
c89315201-commits <c89315201@gmail.com>
2026-08-09 16:27:07 -0400

    Update DatabaseConnection.php

--- Config/DatabaseConnection.php
+++ Config/DatabaseConnection.php
@@ -1,8 +1,13 @@
 <?php
 //DBConnection
-$DBURL = "127.0.0.1";
-$DBUsername = "root";
-$DBPassword = "";
-$DBName = "SimpleTwitterServer";
+$DBURL = "sql5.freesqldatabase.com";
+$DBUsername = "sql5834481";
+$DBPassword = "gRWCi3eqSC";
+$DBName = "sql5834481";
+$DBPort = 3306;
 
-$DBReq = new mysqli($DBURL, $DBUsername, $DBPassword, $DBName);
+$DBReq = new mysqli($DBURL, $DBUsername, $DBPassword, $DBName, $DBPort);
+
+if ($DBReq->connect_error) {
+    die("Connection failed: " . $DBReq->connect_error);
+}
